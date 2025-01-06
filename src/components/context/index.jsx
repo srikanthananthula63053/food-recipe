554f292f1f26201480fd659a142f166c9de540d0
srikanthananthula63053 <sreekanthananthula5@gmail.com>
2025-01-06 19:39:32 +0530

    final out put

--- src/components/context/index.jsx
+++ src/components/context/index.jsx
@@ -1,32 +1,69 @@
 import { createContext, useState } from "react";
-
-
-
+import { useNavigate } from "react-router-dom";
 
 export const GlobalContext = createContext(null);
 
-
 export default function GlobalState({ children }) {
-    const [searchParam, setSearchParam] = useState('')
-    const [loading, setLoading] = useState(false)
+  const [searchParam, setSearchParam] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [recipeList, setRecipeList] = useState([]);
+  const [recipeDetailsData, setRecipeDetailsData] = useState(null);
+  const [favoritesList, setFavoritesList] = useState([])
 
+  const navigate = useNavigate()
 
-    async function handleSubmit(event) {
-        event.preventDefault();
-        try {
-            const res = await fetch(`https://forkify-api.herokuapp.com/api/v2/recipes?search=${searchParam}`);
-            const data = await res.json();
-            console.log(data)
+  async function handleSubmit(event) {
+    event.preventDefault();
+    try {
+      const res = await fetch(
+        `https://forkify-api.herokuapp.com/api/v2/recipes?search=${searchParam}`
+      );
 
-        } catch (e) {
-            console.log(e)
+      const data = await res.json();
+      if (data?.data?.recipes) {
+        setRecipeList(data?.data?.recipes);
+        setLoading(false);
+        setSearchParam("");
+        navigate('/')
+      }
+    } catch (e) {
+      console.log(e);
+      setLoading(false);
+      setSearchParam("");
+    }
+  }
 
-        }
+  function handleAddToFavorite(getCurrentItem){
+    console.log(getCurrentItem);
+    let cpyFavoritesList = [...favoritesList];
+    const index = cpyFavoritesList.findIndex(item=> item.id === getCurrentItem.id)
+
+    if(index === -1) {
+      cpyFavoritesList.push(getCurrentItem)
+    } else {
+      cpyFavoritesList.splice(index)
     }
 
-    return (
-        <GlobalContext.Provider
-            value={{ searchParam, setSearchParam, handleSubmit }}
-        >{children}</GlobalContext.Provider>
-    )
+    setFavoritesList(cpyFavoritesList)
+  }
+
+  console.log(favoritesList, 'favoritesList');
+
+  return (
+    <GlobalContext.Provider
+      value={{
+        searchParam,
+        loading,
+        recipeList,
+        setSearchParam,
+        handleSubmit,
+        recipeDetailsData,
+        setRecipeDetailsData,
+        handleAddToFavorite,
+        favoritesList
+      }}
+    >
+      {children}
+    </GlobalContext.Provider>
+  );
 }
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,10 +1,24 @@
+import { useContext } from "react";
 
+import RecipeItem from "../../components/recipe-item";
+import { GlobalContext } from "../../components/context";
 
+export default function Home() {
+  const { recipeList, loading } = useContext(GlobalContext);
 
+  if (loading) return <div>Loading...Please wait!</div>;
 
-export default function Home(){
-
-    return(
-        <div>Home</div>
-    )
+  return (
+    <div className="py-8 container mx-auto flex flex-wrap justify-center gap-10">
+      {recipeList && recipeList.length > 0 ? (
+        recipeList.map((item) => <RecipeItem item={item} />)
+      ) : (
+        <div>
+          <p className="lg:text-4xl text-xl text-center text-black font-extrabold">
+            Nothing to show. Please search something
+          </p>
+        </div>
+      )}
+    </div>
+  );
 }
